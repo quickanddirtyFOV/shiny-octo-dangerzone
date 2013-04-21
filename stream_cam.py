d3845ab9bad6b7f962fc940ce95723eeee552858
Filip Johansson <userforf1l1p@gmail.com>
2013-04-21 20:28:26 +0200

    Cleanup

--- stream_cam.py
+++ stream_cam.py
@@ -4,15 +4,14 @@ import cv
 
 if __name__ == "__main__":
     cv_cap = cv.CaptureFromCAM(0)
-    
     cv.NamedWindow("stream_win")
 
     while True:
         im = cv.QueryFrame(cv_cap)
-        cv.Laplace(im,im,5)
+        cv.Laplace(im, im, 5)
         cv.ShowImage("stream_win", im)
         if (cv.WaitKey(1) == 27):
-            cv.ReleaseCapture(cv_cap)
+            del(cv_cap)
             break
 
     cv.DestroyAllWindows()
--- stream_cam2.py
+++ stream_cam2.py
@@ -2,35 +2,52 @@
 
 import cv2
 import numpy as np
-from PIL import ImageGrab
+import screengrab
 import cv2.cv as cv
 
-if __name__ == "__main__":
-    chess_im = cv2.imread('chess.png')
-    chess_im = cv2.cvtColor(chess_im, cv2.COLOR_RGB2GRAY)
-    cv_cap = cv2.VideoCapture(0)
-    
+
+def main_loop(cv_cap):
     cv2.namedWindow("stream_win")
     cv2.namedWindow("desktop")
     cv2.namedWindow("cam_im")
-    cv_cap.open(-1)
-    
+
+    chess_im = cv2.imread('chess.png')
+    chess_im = cv2.cvtColor(chess_im, cv2.COLOR_RGB2GRAY)
+
     while True:
-        pi = ImageGrab.grab()
-        #im_cv = np.asarray(pi)[:,:,::-1].copy() # Transform ImageGrab image to CV2 standard
-        im_screen = cv2.cvtColor(np.asarray(pi), cv2.COLOR_RGB2BGR) # Transform ImageGrab image to CV2 standard
-        
+        #pi = ImageGrab.grab()
+        # Transform ImageGrab image to CV2 standard
+        #im_screen = cv2.cvtColor(np.asarray(pi), cv2.COLOR_RGB2BGR)
+
         r, im = cv_cap.read()
-        
-        im_split = cv2.split(im)[2] # extracts the red color channel.
+
+        # extracts the red color channel.
+        im_split = cv2.split(im)[2]
         r, imtresh = cv2.threshold(im_split, 150, 255, cv2.THRESH_BINARY)
-        
+
         im_harris = cv2.cornerHarris(chess_im, 5, 1, 0.04)
         cv2.imshow("stream_win", im_harris)
         cv2.imshow("desktop", chess_im)
         cv2.imshow("cam_im", im)
         if (cv2.waitKey(10) == 27):
-            cv_cap.release()
             break
 
+
+def cleanup(cv_cap):
+    cv_cap.release()
     cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    cv_cap = cv2.VideoCapture()
+
+    cv_cap.open(-1)
+
+    if (not cv_cap.isOpened()):
+        cleanup(cv_cap)
+
+    try:
+        main_loop(cv_cap)
+    except Exception, e:
+        raise e
+    finally:
+        cleanup(cv_cap)
